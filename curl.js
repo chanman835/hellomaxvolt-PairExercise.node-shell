09473e856fdb9fbd145f23adba5750bb57b65bf1
Max Rivera <hellomauriciorivera@gmail.com>
2020-09-25 16:36:10 -0400

    until the end of session

--- curl.js
+++ curl.js
@@ -1,1 +1,18 @@
-let request = require('request')
+let request = require("request");
+
+// request("http://www.google.com", function (error, response, body) {
+//   console.error("error:", error); // Print the error if one occurred
+//   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+//   console.log("body:", body); // Print the HTML for the Google homepage.
+// });
+
+module.exports = function (url) {
+  process.stdout.write(
+    request(url, function (error, response, body) {
+      console.error("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      console.log("body:", body); // Print the HTML for the Google homepage.
+    })
+  );
+  process.stdout.write("\nprompt > ");
+};
